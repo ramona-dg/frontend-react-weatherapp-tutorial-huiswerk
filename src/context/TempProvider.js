72cc9cbae5b9e8bcc72b7eaded58e0ab5ef3659c
ramona-dg <ramona.rdg@gmail.com>
2022-11-16 12:34:00 +0100

    implement useContext for TempProvider

--- src/context/TempProvider.js
+++ src/context/TempProvider.js
@@ -1,26 +1,25 @@
-import React, { createContext } from "react";
-import kelvinToFahrenheit from "../helpers/kelvinToFahrenheit";
-import kelvinToCelsius from "../helpers/kelvinToCelsius";
+import React, { createContext, useState } from 'react';
+import kelvinToCelsius from '../helpers/kelvinToCelsius';
+import kelvinToFahrenheit from '../helpers/kelvinToFahrenheit';
 
 export const TempContext = createContext(null);
 
-function TempContextProvider({ children }){
+function TempContextProvider({ children }) {
     const [selectedMetric, toggleSelectedMetric] = useState('celsius');
 
     function toggleTemp() {
         if (selectedMetric === 'celsius') {
-            toggleSelectedMetric('fahrenheit');celsius
+            toggleSelectedMetric('fahrenheit');
         } else {
             toggleSelectedMetric('celsius');
         }
     }
 
-    return(
+    return (
         <TempContext.Provider value={{
             toggleTemp: toggleTemp,
             kelvinToMetric: selectedMetric === 'celsius' ? kelvinToCelsius : kelvinToFahrenheit,
-        }}
-        >
+        }}>
             { children }
         </TempContext.Provider>
     )
